03265262cabe993bab8489a38f1c57d8a3a4b8ab
Erdem <erdemsincer123@icloud.com>
2025-03-30 16:55:41 +0300

    Apiler yazılmaya başlandı

---
+++ EducationPlatform.Api/Controllers/CategoryController.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using EducationPlatform.Application.Abstract;
+using EducationPlatform.Domain.Entities;
+using EducationPlatform.Dto.CategoryDto;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EducationPlatform.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _categoryService.TGetListAllAsync();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var values = _mapper.Map<Category>(createCategoryDto);
+            await _categoryService.TAddAsync(values);
+            return Ok("Eklendi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            var values = _mapper.Map<Category>(updateCategoryDto);
+            await _categoryService.TUpdateAsync(values);
+            return Ok("Güncellendi");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var value = await _categoryService.TGetByIdAsync(id);
+            if (value == null)
+            {
+                return NotFound("Kategori bulunamadı");
+            }
+
+            await _categoryService.TDeleteAsync(value);
+            return Ok("Silindi");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdCategory(int id)
+        {
+            var value = await _categoryService.TGetByIdAsync(id);
+            if (value == null)
+            {
+                return NotFound("Kategori bulunamadı");
+            }
+
+            return Ok(value);
+        }
+
+        [HttpGet("GetByName")]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            var value = await _categoryService.GetByNameAsync(name);
+            if (value == null)
+            {
+                return NotFound("Kategori bulunamadı");
+            }
+
+            return Ok(value);
+        }
+    }
+}
