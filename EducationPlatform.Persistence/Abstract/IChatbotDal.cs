e1e545d27879c05e1f7fee2248ccdaf13b632eed
Erdem <erdemsincer123@icloud.com>
2025-05-16 21:10:26 +0300

    chatbot dal

---
+++ EducationPlatform.Persistence/Abstract/IChatbotDal.cs
@@ -0,0 +1,8 @@
+﻿using EducationPlatform.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IChatbotDal
+{
+    Task<User> GetUserWithDetailsAsync(int userId);
+}
---
+++ EducationPlatform.Persistence/EntityFrameworkCore/EFChatbotDal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EducationPlatform.Domain.Entities;
+using EducationPlatform.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+public class EFChatbotDal : IChatbotDal
+{
+    private readonly ApplicationDbContext _context;
+
+    public EFChatbotDal(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User> GetUserWithDetailsAsync(int userId)
+    {
+        return await _context.Users
+            .Include(u => u.Skills)
+            .Include(u => u.Interests)
+            .Include(u => u.CareerGoals)
+            .FirstOrDefaultAsync(u => u.Id == userId);
+    }
+}
