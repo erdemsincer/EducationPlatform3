6b1d37d18d30363408cd265f801c6e050537d26f
Erdem <erdemsincer123@icloud.com>
2025-05-16 21:12:33 +0300

    career servis

---
+++ EducationPlatform.Persistence/Abstract/ICareerTestDal.cs
@@ -0,0 +1,13 @@
+﻿using EducationPlatform.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EducationPlatform.Persistence.Abstract
+{
+    public interface ICareerTestDal
+    {
+        Task<List<CareerTest>> GetAllQuestionsAsync(); // Soruları getir
+        Task SaveUserAnswersAsync(List<CareerTest> careerTests); // Kullanıcı cevaplarını kaydet
+        Task<List<CareerTest>> GetUserAnswers(int userId); // Kullanıcının verdiği yanıtları getir
+    }
+}
---
+++ EducationPlatform.Persistence/EntityFrameworkCore/EFCareerTestQuestionDal.cs
@@ -0,0 +1,32 @@
+﻿using EducationPlatform.Domain.Entities;
+using EducationPlatform.Persistence.Abstract;
+using EducationPlatform.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationPlatform.Persistence.EntityFrameworkCore
+{
+    public class EFCareerTestQuestionDal : ICareerTestQuestionDal
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EFCareerTestQuestionDal(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CareerTestQuestion>> GetAllQuestionsAsync()
+        {
+            return await _context.CareerTestQuestions.ToListAsync();
+        }
+
+        public async Task<CareerTestQuestion> GetQuestionByIdAsync(int questionId)
+        {
+            return await _context.CareerTestQuestions.FindAsync(questionId);
+        }
+    }
+}
