e3b011699854ba99b7f14d3072f267cfd7d60075
Erdem <erdemsincer123@icloud.com>
2025-05-24 15:51:39 +0300

    student layout

---
+++ EducationPlatform.WebUI/Areas/Student/Controllers/StudentLayoutController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EducationPlatform.WebUI.Areas.Student.Controllers
+{
+    public class StudentLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult StudentHeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult StudentNavBarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult StudentSideBarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult StudentScriptPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult StudentFooterPartial()
+        {
+            return PartialView();
+        }
+    }
+}
