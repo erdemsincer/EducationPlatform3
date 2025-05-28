e244d75c51c34e301203c11bf365cdf4bcacbfe9
Erdem <erdemsincer123@icloud.com>
2025-05-28 23:23:29 +0300

    cariyer servis

---
+++ EducationPlatform.Application/Abstract/ICareerTestAnswerService.cs
@@ -0,0 +1,14 @@
+﻿using EducationPlatform.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EducationPlatform.Application.Abstract
+{
+    public interface ICareerTestAnswerService
+    {
+        Task SaveUserAnswersAsync(int userId, Dictionary<int, string> answers); // Kullanıcı cevaplarını kaydet
+        Task<List<CareerTestAnswer>> GetUserAnswersAsync(int userId); // Kullanıcının verdiği cevapları getir
+        Task<string> GetCareerAdviceAsync(int userId); // Kullanıcının cevaplarına göre öneri al
+    }
+}
---
+++ EducationPlatform.Application/Abstract/ICareerTestQuestionService.cs
@@ -0,0 +1,15 @@
+﻿using EducationPlatform.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationPlatform.Application.Abstract
+{
+    public interface ICareerTestQuestionService
+    {
+        Task<List<CareerTestQuestion>> GetAllQuestionsAsync(); // Tüm test sorularını getir
+        Task<CareerTestQuestion> GetQuestionByIdAsync(int questionId); // ID'ye göre test sorusu getir
+    }
+}
