12e8ce1fabb0e0cceb99ba83c96b71430418726b
Erdem <erdemsincer123@icloud.com>
2025-05-28 23:23:47 +0300

    admin about

---
+++ EducationPlatform.WebUI/Areas/Admin/Controllers/AdminAboutController.cs
@@ -0,0 +1,100 @@
+﻿using EducationPlatform.Dto.AboutDto;
+using EducationPlatform.Dto.AboutDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace EducationPlatform.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminAbout")]
+    public class AdminAboutController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminAboutController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var values = await client.GetFromJsonAsync<List<ResultAboutDto>>("http://localhost:7028/api/About");
+            return View(values);
+        }
+
+        [HttpGet]
+        [Route("CreateAbout")]
+        public IActionResult CreateAbout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateAbout")]
+        public async Task<IActionResult> CreateAbout(CreateAboutDto createAboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.PostAsJsonAsync("http://localhost:7028/api/About", createAboutDto);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Hakkımızda eklenirken hata oluştu!");
+            return View(createAboutDto);
+        }
+        [HttpPost]
+        [Route("DeleteAbout")]
+        public async Task<IActionResult> DeleteAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:7028/api/About/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Silme işlemi sırasında hata oluştu!");
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        [Route("UpdateAbout/{id}")]
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:7028/api/About/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var about = JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData);
+                return View(about);
+            }
+            return View(new UpdateAboutDto());
+        }
+
+        [HttpPost]
+        [Route("UpdateAbout/{id}")]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            Console.WriteLine("Güncellenen JSON: " + JsonConvert.SerializeObject(updateAboutDto));
+            var responseMessage = await client.PutAsJsonAsync($"http://localhost:7028/api/About", updateAboutDto);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "About güncellenirken hata oluştu!");
+            return View(updateAboutDto);
+        }
+
+    }
+}
