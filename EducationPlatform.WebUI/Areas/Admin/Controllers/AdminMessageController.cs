c5c24e48a1bb7329bedbd840cb5556350e524293
Erdem <erdemsincer123@icloud.com>
2025-05-28 23:24:17 +0300

    admin message

---
+++ EducationPlatform.WebUI/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,61 @@
+﻿using EducationPlatform.Dto.MessageDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace EducationPlatform.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminMessage")]
+    public class AdminMessageController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminMessageController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+       
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var values = await client.GetFromJsonAsync<List<ResultMessageDto>>("http://localhost:7028/api/Message");
+            return View(values);
+        }
+
+        
+        [HttpGet]
+        [Route("MessageDetail/{id}")]
+        public async Task<IActionResult> MessageDetail(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:7028/api/Message/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var message = JsonConvert.DeserializeObject<ResultMessageDto>(jsonData);
+                return View(message);
+            }
+
+            return RedirectToAction("Index"); // Mesaj bulunamazsa listelemeye yönlendir
+        }
+
+        [HttpPost]
+        [Route("RemoveMessage")]
+        public async Task<IActionResult> RemoveMessage(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:7028/api/Message/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Mesaj silinirken hata oluştu!");
+            return RedirectToAction("Index");
+        }
+    }
+}
